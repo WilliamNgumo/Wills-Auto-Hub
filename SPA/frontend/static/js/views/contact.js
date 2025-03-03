35c1d86d14c8063467a4872c2a43abe745216aae
WilliamNgumo <ngumpwilliam@gmail.com>
2025-03-03 16:44:15 -0600

    Backend SPA Set Up
    Got the Views going, Fixed the .json Packages that kept duplicating on the backend, added server side.

---
+++ SPA/frontend/static/js/views/contact.js
@@ -0,0 +1,13 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+    constructor() {
+        super();
+        this.setTitle("contact");
+    };
+    async getHtml() {
+        return `
+            <h1 class="brother"> This Is contact Us </h1>
+        `;
+    };
+}
